bff10626ff804e7d309d12970f7084ed5902907e
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2021-05-05 17:35:35 +0200

    args reorganized

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
+from utils import DeckDownloader, DeckSender
 import json
 import os
-import sys
-from utils import DeckDownloader, DeckSender
+import argparse
 
 FILE_NAME = 'nextcloud-decks.json'
 
-mode = sys.argv[1] if len(sys.argv) > 1 else None
-host = sys.argv[2] if len(sys.argv) > 2 else None
-username = sys.argv[3] if len(sys.argv) > 3 else None
-passwd = sys.argv[4] if len(sys.argv) > 4 else None
+parser = argparse.ArgumentParser()
 
-urlFrom = os.getenv('NEXTCLOUD_HOST') or host
-authFrom = (os.getenv('NEXTCLOUD_USER') or username, os.getenv(
-    'NEXTCLOUD_PASSWORD') or passwd)
+parser.add_argument('--mode')
+parser.add_argument('--host', default="http://localhost:8080")
+parser.add_argument('--username', default="test")
+parser.add_argument('--passwd', default="test")
+parser.add_argument('--directory', default=".")
 
-urlTo = os.getenv('NEXTCLOUD_HOST') or host or 'http://localhost:8080'
-authTo = (os.getenv('NEXTCLOUD_USER') or username or 'test', os.getenv(
-    'NEXTCLOUD_PASSWORD') or passwd or 'test')
+args = parser.parse_args()
 
-if((mode == 'backup' or mode == None) and (urlFrom != None and authFrom[0] != None and authFrom[0] != None)):
-    dd = DeckDownloader(urlFrom, authFrom)
+mode = args.mode
+url = args.host
+auth = (args.username, args.passwd)
+
+
+if(mode == 'backup' or mode == None):
+    dd = DeckDownloader(url, auth)
 
     data = dd.fetchBoards()
 
-    with open(FILE_NAME, 'w') as outfile:
+    with open(os.path.join(args.directory, FILE_NAME), 'w') as outfile:
         json.dump(data, outfile, indent=4)
 
 elif(mode == 'send'):
-    ds = DeckSender(urlTo, authTo)
+    ds = DeckSender(url, auth)
     # ds.sendBoard(data)
 else:
     raise ValueError('Unknown mode or missing properites.')
